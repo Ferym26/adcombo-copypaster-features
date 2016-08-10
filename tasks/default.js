74c81addf178bbcaba1e27439b8f7907b9ab719e
Badalik <badaliki@yandex.ru>
2016-08-10 18:49:09 +0300

    feat(tasks) add gulp-jsbeautifier for prettify css edit(index.html) add new item to list

--- tasks/default.js
+++ tasks/default.js
@@ -3,6 +3,7 @@ var uncss = require('gulp-uncss');
 var nano = require('gulp-cssnano');
 var gulpIf = require('gulp-if');
 var autoprefixer = require('gulp-autoprefixer');
+var prettify = require('gulp-jsbeautifier');
 
 const isDebug = process.env.NODE_ENV !== 'production';
 
@@ -10,6 +11,11 @@ gulp.task('default', function () {
     return gulp.src('styles/*.css')
         .pipe(uncss({
             html: ['index.html']
+        }))
+        .pipe(prettify({
+            indent_with_tabs: true,
+            selector_separator_newline: true,
+            newline_between_rules: true
         }))
         .pipe(autoprefixer())
         .pipe(gulpIf(!isDebug, nano({zindex: false})))
