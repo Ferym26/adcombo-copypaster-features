2d9b3a04b7d8cec45ccd429b892a3abebe87b0ce
Badalik <badaliki@yandex.ru>
2016-08-11 11:21:48 +0300

    feat(tasks) add gulp-jsbeautifier for html edit(tasks) divided styles and templates tasks edit(index.html) edit list edit(readme.md) edit description

--- tasks/default.js
+++ tasks/default.js
@@ -1,23 +1,9 @@
+var runSequence = require('run-sequence');
 var gulp = require('gulp');
-var uncss = require('gulp-uncss');
-var nano = require('gulp-cssnano');
-var gulpIf = require('gulp-if');
-var autoprefixer = require('gulp-autoprefixer');
-var prettify = require('gulp-jsbeautifier');
 
-const isDebug = process.env.NODE_ENV !== 'production';
-
-gulp.task('default', function () {
-    return gulp.src('styles/*.css')
-        .pipe(uncss({
-            html: ['index.html']
-        }))
-        .pipe(prettify({
-            indent_with_tabs: true,
-            selector_separator_newline: true,
-            newline_between_rules: true
-        }))
-        .pipe(autoprefixer())
-        .pipe(gulpIf(!isDebug, nano({zindex: false})))
-        .pipe(gulp.dest('./styles'));
-});
+gulp.task('default', () => (
+    runSequence(
+        'styles',
+        'templates'
+    )
+));
